b93cf166e66d9797b24c90538c14d7b05bd264c1
Cassia Miller <mill1713@msu.edu>
2011-03-26 22:07:10 -0400

    fixed the filter

--- public/js/need_filter.js
+++ public/js/need_filter.js
@@ -1,11 +1,11 @@
 $(function() {
   function filterNeeds(){
     if($("#filterText").val() != ""){ 
-      $('li').filter("li:not(contains("+$("#filterText").val()+"))").css("display","none");
-      $('li').filter("li:contains("+$("#filterText").val()+")").css("display","");
+      $('li.need').filter("li:not(contains("+$("#filterText").val()+"))").css("display","none");
+      $('li.need').filter("li:contains("+$("#filterText").val()+")").css("display","");
     }
     else{
-      $('li').filter("li").css("display", "");
+      $('li.need').filter("li").css("display", "");
     }
     return false;
   }
