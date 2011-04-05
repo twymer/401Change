9baa512551305aa0940e84d9d3fd04fc3cb7121e
Zachary Spencer <zspencer@zacharyspencer.com>
2011-04-04 22:07:26 -0400

    made it so the cucumber uses the dev environment :/

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,4 @@
-PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
+PADRINO_ENV = 'development' unless defined?(PADRINO_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
 
 require 'capybara/cucumber'
